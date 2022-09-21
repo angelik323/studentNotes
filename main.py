a42eb3bee9549a6d12a7dc2cb2f9c373c324665d
angelik323 <angelica.loaiza323@gmail.com>
2022-09-20 22:05:51 -0500

    Acuerdo de estructura: Nos ponemos de acuerdo como equipo que la estructura se manejará como la imagen subida

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-from flask import Flask
+from flask import Flask, redirect, request, make_response, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    user_ip = request.remote_addr
+
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+
+    return response 
+
+@app.route('/hello')
 def hello():
-    return "Hello world Flask"
+    #return "Hello world Flask hoy mas"
+    user_ip = request.cookies.get('user_ip')
+    return 'Esta es tu dirección IP desde donde nos visitas: {}'.format(user_ip)
+    #Otra manera de dar formato es:
+        #return f'Hello World Gente, tu IP es{user_ip}'
+
+#las siguientes dos lineas sirven para debuggear el servidor y acepte cambios al instante sin parar el server
+if __name__ == '__main__':
+    app.run(debug=True)
 app.run()
