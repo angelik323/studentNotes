e5685aefb6ae9243de06ca1ea814a92759241e3d
angelik323 <angelica.loaiza323@gmail.com>
2022-09-21 22:42:23 -0500

    Creación de vistas: Se crea login - vista de studiantes, docentes y administrador

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, request, make_response, redirect
+from flask import Flask, redirect, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
@@ -6,18 +6,30 @@ app = Flask(__name__)
 def index():
     user_ip = request.remote_addr
 
-    response = make_response(redirect('/hello'))
+    response = make_response(redirect('/login'))
     response.set_cookie('user_ip', user_ip)
 
     return response 
 
-@app.route('/hello')
-def hello():
+@app.route('/login')
+def login():
     #return "Hello world Flask hoy mas"
-    user_ip = request.cookies.get('user_ip')
-    return 'Esta es tu dirección IP desde donde nos visitas: {}'.format(user_ip)
+    #user_ip = request.cookies.get('user_ip')
+    return render_template('login.html')
     #Otra manera de dar formato es:
         #return f'Hello World Gente, tu IP es{user_ip}'
+
+@app.route('/student')
+def student():
+    return render_template('student.html')
+
+@app.route('/teachers')
+def teachers():
+    return render_template('teachers.html')
+
+@app.route('/admon')
+def admon():
+    return render_template('admon.html')
 
 #las siguientes dos lineas sirven para debuggear el servidor y acepte cambios al instante sin parar el server
 if __name__ == '__main__':
